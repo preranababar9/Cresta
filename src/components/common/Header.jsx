bcdf939f751b8935039ef11e1975b54098e2026d
Preranabrainvire <prerna.babar@brainvire.com>
2024-07-25 22:28:02 +0530

    header css

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,28 +4,30 @@ import announce from "/home/announce.svg";
 import socials from "/home/socials.svg";
 const Header = () => {
   return (
-    <header className="w-full sticky h-[144px] top-0 bg-pink-100">
+    <header className="w-full max-w sticky h-12vh top-0 bg-pink-100 flex">
 
         {/* logo div */}
-      <div className=" bg-[#3F74EF] w-44 h-[144px] flex justify-center items-center">
+      <div className=" bg-[#3F74EF] w-44 h-12vh flex justify-center items-center">
         <img src={logo1} alt="logo" className="w-[138px] h-[68px]" />
       </div>
 
 {/* info  */}
-      <div className="flex">
+      <div className=" px-6 w-full" >
 
         {/* announcement */}
-        <div>
-          <img src={announce} alt="anouncement" className="" />
-            <p>Sunday-Friday:</p>
+        <div className="flex justify-between items-center h-3vh bg-blue-300" >
+          <div className="flex gap-2">
+          <img src={announce} alt="anouncement" className="h-6 w-6" />
+            <p className="font-bold text-md">Sunday-Friday :</p>
             <p>9 am- 8pm</p>
+            </div>
 
-          <img src={socials} alt="socials" />
+          <img src={socials} alt="socials" className="w-32 h-5"/>
         </div>
 
 {/* navlinks */}
-        <div className="flex">
-          <ul className="flex ">
+        <div className="flex justify-between items-center bg-yellow-700 h-9vh ">
+          <ul className=" flex gap-10 items-center">
             <li><a>Home</a></li>
             <li><a>About Us</a></li>
             <li><a>Services</a></li>
