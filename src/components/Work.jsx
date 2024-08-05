d007ebdab8a7e2258e2ccd9e3963131a1964549d
Preranabrainvire <prerna.babar@brainvire.com>
2024-08-05 16:27:51 +0530

    Section work done

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -20,10 +20,10 @@ const Work = () => {
 
 {/* 
 1st div */}
-        <div className='flex gap-5 w-4/5 '>
+        <div className='flex gap-5 px-12'>
 
                 <div className='w-1/3 relative bottom-12 '>
-                        <div className="flex items-center justify-center absolute left-36 -top-8">
+                        <div className="flex items-center justify-center absolute left-44 -top-8">
                         <img src={work1} alt="icon" className='bg-blue-700 py-2 px-2 rounded-md' />
                         </div>
 
@@ -37,7 +37,7 @@ const Work = () => {
 
                 {/* 2nd  */}
                 <div className='w-1/3 relative bottom-12'>
-                        <div  className="flex items-center justify-center absolute left-36 -top-8">
+                        <div  className="flex items-center justify-center absolute left-44 -top-8">
                         <img src={work2} alt="" className='bg-blue-700 py-2 px-2 rounded-md' />
                          </div>
 
@@ -50,7 +50,7 @@ const Work = () => {
 
                 {/* 3rd */} 
                 <div className='w-1/3 relative bottom-12 '>
-                        <div  className="flex items-center justify-center absolute left-36 -top-8">
+                        <div  className="flex items-center justify-center absolute left-44 -top-8">
                         <img src={work3} alt="" className='bg-blue-700 py-2 px-2 rounded-md' />
                         </div>
 
