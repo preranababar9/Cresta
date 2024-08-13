329ff9580b850be17382a540b8afdb0ff17b1815
Preranabrainvire <preranababar9403@gmail.com>
2024-08-13 18:09:53 +0530

    counter section responsive

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,13 +7,13 @@ import count4 from "/icons/count4.svg";
 const Counter = () => {
   return (
         <section className='py-20 bg-[#351EE0]'>
-    <div className='max-width w-full flex max-md:flex-col'>
-<div className='w-1/2 ' >
+    <div className='max-width w-full flex max-md:flex-col max-md:gap-10'>
+<div className='lg:w-1/2 ' >
 <p className="text-sm font-bold  text-white uppercase pl-2 pb-3">
         business
           </p>
-          <h2 className="text-white text-5xl  max-md:text-4xl font-semibold pb-10">
-          Stay Ahead with <br></br>the Cutting-Edge IT
+          <h2 className="text-white text-5xl lg:w-2/3 w-full max-md:text-4xl  font-semibold pb-10">
+          Stay Ahead with the Cutting-Edge IT
           </h2>
 
           <button className="bg-[#351EE0] border-solid border-2 border-white hover:bg-blue-500 text-white font-medium py-3 px-6">
@@ -21,13 +21,13 @@ const Counter = () => {
           </button>
 </div>
 
-<div className='w-1/2 flex max-md:flex-col flex-wrap justify-evenly'>
+<div className='lg:w-1/2   flex  max-md:gap-4 flex-wrap justify-evenly '>
 
 {count.map((item, index) => (
- <div className='flex gap-5 w-[50%] items-center'>
- <img src={item.img} alt="" className='h-16' />
+ <div className='flex  gap-5 lg:w-[50%] relative items-center'>
+ <img src={item.img} alt="" className='h-14 absolute -left-6' />
 
- <div className='flex flex-col items-start border-solid border-2 border-white py-4 px-3 '>
+ <div className='flex flex-col lg:items-start border-solid border-2  lg:w-2/3 w-[180px]  border-white py-4 px-8 '>
  <p className='text-white text-4xl font-bold'> {item.num}</p>
  <p className='text-white text-sm '>{item.text}</p>
  </div>
