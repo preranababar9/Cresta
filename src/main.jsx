983872a670e2782232ae1766dbd6f8aabfc44e93
Preranabrainvire <preranababar9403@gmail.com>
2024-08-20 17:56:41 +0530

    react router dom installed and pages and links created

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from './pages/Home.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "home",
+    element: <Home/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Hero from '../components/home/Hero'
 import Features from '../components/home/Features'
+import Header from '../components/common/Header'
+import Footer from '../components/common/Footer'
 
 
 const Home = () => {
   return (
     <div>
+      <Header/>
         <Hero/>
         <Features/>
+        <Footer/>
         
     </div>
   )
