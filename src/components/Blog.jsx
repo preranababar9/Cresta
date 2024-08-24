71c24e4a79fc0cc391f67013fe386a3b3e633b9c
Preranabrainvire <preranababar9403@gmail.com>
2024-08-24 20:53:30 +0530

    blog post responsive

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -18,32 +18,39 @@ const Blog = () => {
           </h1>
         </div>
 
-        <div className="w-full flex max-md:flex-col max-md:gap-10 justify-evenly flex-wrap ">
+        <div className="w-full flex max-md:flex-col max-md:gap-10 items-center justify-evenly flex-wrap ">
+          {blog.map((item, index) => (
+            <div
+              key={index}
+              className="lg:w-[30%] border-solid border-2 py-5 border-gray-300 w-[90vw] flex flex-col items-center   lg:hover:scale-105 lg:duration-300"
+            >
+              <img src={item.img} alt="" className="w-3/4" />
 
-                {blog.map((item, index) => (
-                           <div key={index} className="lg:w-[30%]   lg:hover:scale-105 lg:duration-300">
-                           <img src={item.img} alt="" className=""/>
-               
-                           <div className="lg:hover:bg-blue-300  flex justify-around py-2">
-                             <div className="flex items-center gap-2 ">
-                               <img src={item.comimg} alt="" className="h-5" />
-                               <p className=" text-black ">{item.textcom}</p>
-                             </div>
-               
-                             <div className="flex items-center gap-2">
-                               {" "}
-                               <img src={item.adminimg} alt="" className="h-5 object-cover" />
-                               <p className=" text-black">{item.admintext}</p>
-                             </div>
-                           </div>
-               
-                           <h4 className=" lg:text-lg text-md font-bold px-16 pt-6">
-                            {item.head}
-                           </h4>
-                           <button className=" text-md font-bold px-16 pt-6">{item.button}</button>
-                         </div>
-                ))}
-       
+              <div className="lg:hover:bg-blue-300 lg:w-3/4 w-full flex justify-around py-2">
+                <div className="flex items-center lg:gap-2 gap-1 ">
+                  <img src={item.comimg} alt="" className="h-5" />
+                  <p className=" text-black ">{item.textcom}</p>
+                </div>
+
+                <div className="flex items-center gap-2">
+                  {" "}
+                  <img
+                    src={item.adminimg}
+                    alt=""
+                    className="h-5 object-cover"
+                  />
+                  <p className=" text-black">{item.admintext}</p>
+                </div>
+              </div>
+
+              <h4 className=" lg:text-lg text-center text-md font-bold px-16 pt-6">
+                {item.head}
+              </h4>
+              <button className="bg-blue-300 text-md font-bold px-10 py-3 mt-3">
+                {item.button}
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </section>
@@ -53,31 +60,31 @@ const Blog = () => {
 export default Blog;
 
 const blog = [
-        {
-                img : blogimg,
-                comimg : comment,
-                textcom : "Comments(3)",
-                adminimg : admin,
-                admintext : "By Admin",
-                head : " Unleash the Power of IT Unlock Your Business",
-                button : "Read More"
-        },
-        {
-                img : blogimg,
-                comimg : comment,
-                textcom : "Comments(5)",
-                adminimg : admin,
-                admintext : "By Admin",
-                head : " Experience the Future of IT the Simplify IT for Enhanced",
-                button : "Read More"
-        },
-        {
-                img : blogimg,
-                comimg : comment,
-                textcom : "Comments(10)",
-                adminimg : admin,
-                admintext : "By Admin",
-                head : "Stay Ahead with Cutting-Edge IT Solutions",
-                button : "Read More"
-        }
-]
+  {
+    img: blogimg,
+    comimg: comment,
+    textcom: "Comments(3)",
+    adminimg: admin,
+    admintext: "By Admin",
+    head: " Unleash the Power of IT Unlock Your Business",
+    button: "Read More",
+  },
+  {
+    img: blogimg,
+    comimg: comment,
+    textcom: "Comments(5)",
+    adminimg: admin,
+    admintext: "By Admin",
+    head: " Experience the Future of IT the Simplify IT for Enhanced",
+    button: "Read More",
+  },
+  {
+    img: blogimg,
+    comimg: comment,
+    textcom: "Comments(10)",
+    adminimg: admin,
+    admintext: "By Admin",
+    head: "Stay Ahead with Cutting-Edge IT Solutions",
+    button: "Read More",
+  },
+];
